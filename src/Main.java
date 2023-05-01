0d208dc229fe61b9e47cb7655edca8803bed89f2
Hannd15 <lejhernandez08@gmail.com>
2023-05-01 11:43:40 -0500

    Falta comentar, el metodo importarProductos se muere solo falta probar si funcioa lo demás

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Scanner;
 
-        System.out.println("Hello world!");
-        System.out.println("A world!");
+public class Main {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        menu menu = new menu();
+        seleccion seleccion = new seleccion();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            LinkedList productosActuales=seleccion.listar();
+            menu.imprimirMenu();
+            int opcion = Integer.parseInt(sc.nextLine());
+            menu.seleccion(opcion);
+        }
     }
 }
