469c592c38a4af6f25bc294fe5b220364d1de2e0
Hannd15 <lejhernandez08@gmail.com>
2023-05-01 19:48:53 -0500

    Falta el metodo para eliminar productos

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,12 @@ public class Main {
     public static void main(String[] args) throws Exception {
         menu menu = new menu();
         seleccion seleccion = new seleccion();
-        int nums = 0;
         Scanner sc = new Scanner(System.in);
         while (true) {
             seleccion.listar();
             menu.imprimirMenu();
             int opcion = Integer.parseInt(sc.nextLine());
-            if(opcion == 2){
-                nums++;
-            }
-            menu.seleccion(opcion, nums);
+            menu.seleccion(opcion);
         }
     }
 }
