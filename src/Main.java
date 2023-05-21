623933c09e5057db1ea12c8588d0a0a01d4befbd
DanielRHades <drodriguez442@unab.edu.co>
2023-05-21 18:23:31 -0500

    Todo listo

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+
 import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        FlatDarkLaf.setup();
         new seleccion().checkDirectory();
         JOptionPane.showMessageDialog(null, "Bienvenido al sistema de\nalmacenamiento gratuito:\n              U-STOCK", "U-STOCK",1);
         Ventana principal = new Ventana();
